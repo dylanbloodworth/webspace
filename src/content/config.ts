1af179120c8621da48d0fe22bed8853953f9702c
dylanbloodworth <dylan.bloodworth@gmail.com>
2025-07-04 01:07:10 -0400

    added collection for book reviews

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,5 @@
 import { defineCollection, z } from "astro:content"
+import { file } from "astro/loaders";
 
 const blogCollection = defineCollection({
 	type: 'content',
@@ -8,6 +9,16 @@ const blogCollection = defineCollection({
 	}),
 });
 
+const bookReviewsCollection = defineCollection({
+	loader: file("src/data/bookReviews.json"),
+	schema: z.object({
+		id: z.string(),
+		title: z.string(),
+		author: z.string(),
+	}),
+})
+
 export const collections = {
 	blog: blogCollection,
+	bookReviews: bookReviewsCollection
 };
